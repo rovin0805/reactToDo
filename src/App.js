2687da03375350053e93c06ca84385f1793253ab
rovin0805 <qhdrn67@naver.com>
2020-12-05 18:12:03 +0900

    uploaded All files

--- src/App.js
+++ src/App.js
@@ -1,26 +1,69 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useRef, useCallback } from 'react';
+import TodoTemplate from './components/TodoTemplate';
+import TodoInsert from './components/TodoInsert';
+import TodoList from './components/TodoList';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+const App = () => {
+  const [todos, setTodos] = useState([
+    {
+      id: 1,
+      text: '공부하기',
+      checked: true,
+    },
+    {
+      id: 2,
+      text: '운동하기',
+      checked: true,
+    },
+    {
+      id: 3,
+      text: '청소하기',
+      checked: false,
+    },
+  ]);
+
+  // 고유값으로 사용될 id
+  // ref를 사용하여 변수 담기
+  const nextId = useRef(4);
+
+  // props로 전달해야 할 함수 생성시 useCallback를 사용해 함수를 감쌀 것
+  const onInsert = useCallback(
+    (text) => {
+      const todo = {
+        id: nextId.current,
+        text,
+        checked: false,
+      };
+      setTodos(todos.concat(todo));
+      nextId.current += 1;
+    },
+    [todos],
   );
-}
+
+  const onRemove = useCallback(
+    (id) => {
+      setTodos(todos.filter((todo) => todo.id !== id));
+    },
+    [todos],
+  );
+
+  const onToggle = useCallback(
+    (id) => {
+      setTodos(
+        todos.map((todo) =>
+          todo.id === id ? { ...todo, checked: !todo.checked } : todo,
+        ),
+      );
+    },
+    [todos],
+  );
+
+  return (
+    <TodoTemplate>
+      <TodoInsert onInsert={onInsert} />
+      <TodoList todos={todos} onRemove={onRemove} onToggle={onToggle} />
+    </TodoTemplate>
+  );
+};
 
 export default App;
---
+++ src/components/TodoList.js
@@ -0,0 +1,24 @@
+// todos 배열을 props로 받아 온 후,
+// 이를 배열 내장 함수 map을 사용해서
+// 여러 개의 TodoListItem 컴포넌트로 변환하여 보여줌
+
+import React from 'react';
+import './TodoList.scss';
+import TodoListItem from './TodoListItem';
+
+const TodoList = ({ todos, onRemove, onToggle }) => {
+  return (
+    <div className="TodoList">
+      {todos.map((todo) => (
+        <TodoListItem
+          todo={todo}
+          key={todo.id}
+          onRemove={onRemove}
+          onToggle={onToggle}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default TodoList;
